fcb861ff90a14e4a9ca1315bd7e53b60ced12c02
Mihai <coslet.mihai@gmail.com>
2020-07-07 12:11:31 +0300

    configure poetry (a dependency manager)

---
+++ rdf_differ/skos_history_wrapper.py
@@ -0,0 +1,13 @@
+"""
+skos_history_wrapper.py
+Date: 06/07/2020
+Author: Mihai Coșleț
+Email: coslet.mihai@gmail.com
+"""
+
+
+def wrapper():
+    """
+
+    :return:
+    """
---
+++ test/test_skos_history_wrapper.py
@@ -0,0 +1,10 @@
+"""
+test_skos_history_wrapper.py
+Date: 06/07/2020
+Author: Mihai Coșleț
+Email: coslet.mihai@gmail.com
+"""
+
+
+def test_skos_history_wrapper_exists():
+    from rdf_differ.skos_history_wrapper import wrapper
