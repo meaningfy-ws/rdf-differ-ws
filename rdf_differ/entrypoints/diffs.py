c62bd02ed1ad6aef7a8028838bd8dff325867b2f
Mihai <coslet.mihai@gmail.com>
2020-07-30 18:28:58 +0300

    Add the Flask connexion configuration.
    - update openapi.yaml
    - write API methods (diffs.py)

---
+++ rdf_differ/entrypoints/diffs.py
@@ -0,0 +1,55 @@
+"""
+diffs.py
+Date: 30/07/2020
+Author: Mihai Coșleț
+Email: coslet.mihai@gmail.com
+"""
+from rdf_differ.diff_adapter import FusekiDiffAdapter
+
+
+def get_diffs():
+    """
+    List the existent datasets.
+
+    :return: list of datasets
+    """
+    fuseki_adapter = FusekiDiffAdapter('http://localhost:3030')
+    datasets = fuseki_adapter.list_datasets()
+    response = []
+    for dataset in datasets:
+        response.append({dataset: fuseki_adapter.diff_description(dataset)})
+
+    return response
+
+
+def create_diff(dataset_id, dataset_uri, new_version_id, old_version_id, new_version_file_content,
+                new_version_file_name, old_version_file_content, old_version_file_name):
+    """
+
+    :param dataset_id:
+    :param dataset_uri:
+    :param new_version_id:
+    :param old_version_id:
+    :param new_version_file_content:
+    :param new_version_file_name:
+    :param old_version_file_content:
+    :param old_version_file_name:
+    :return:
+    """
+
+
+def get_diff(dataset_id):
+    """
+
+    :param dataset_id:
+    :return:
+    """
+    return dataset_id
+
+
+def delete_diff(dataset_id):
+    """
+
+    :param dataset_id:
+    :return:
+    """
