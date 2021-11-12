154545db6a2006ce8ff53802982ea8bfdb784f7c
Mihai Coslet <coslet.mihai@gmail.com>
2021-11-12 10:57:49 +0200

    add queue documentation

--- rdf_differ/services/queue.py
+++ rdf_differ/services/queue.py
@@ -14,13 +14,25 @@ logger = logging.getLogger(RDF_DIFFER_LOGGER)
 
 
 def stop_task(task_id):
+    """
+    try to add task into the revoking queue
+
+    :param task_id: celery task id
+    """
     if not task_exists_in_revoking_queue(task_id):
         push_task_to_revoking_queue(task_id)
     else:
         raise ValueError('task already marked for revoking')
 
 
-def cleanup_diff_creation(dataset_id, task_id):
+def cleanup_diff_creation(dataset_id, task_id) -> bool:
+    """
+    undo all actions for diff creation
+
+    :param dataset_id: dataset to clean
+    :param task_id: celery task id
+    :return: if task was marked for removal return true else false
+    """
     logger.debug('request to revoke task received. initiating cleanup process.')
     if task_exists_in_revoking_queue(task_id):
         FusekiDiffAdapter(config.RDF_DIFFER_FUSEKI_SERVICE, http_client=requests,
@@ -33,7 +45,17 @@ def cleanup_diff_creation(dataset_id, task_id):
     return False
 
 
-def cleanup_report_creation(dataset_id, application_profile, template_type, db_location, task_id):
+def cleanup_report_creation(dataset_id, application_profile, template_type, db_location, task_id) -> bool:
+    """
+    undo all actions for report creation
+
+    :param dataset_id: dataset name for report identification
+    :param application_profile: application profile for report identification
+    :param template_type: report marked for cleanup
+    :param db_location: which db to be used for cleanup
+    :param task_id:  celery task id
+    :return: if task was marked for removal return true else false
+    """
     logger.debug('request to revoke task received. initiating cleanup process.')
     if task_exists_in_revoking_queue(task_id):
         remove_report(dataset_id, application_profile, template_type, db_location)
