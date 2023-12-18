4155835a06d477fd976dd94470984595e85586a0
Rashif Ray Rahman <schiv@archlinux.org>
2023-12-19 01:00:55 +0600

    Attempt upgrade to Connexion 3
    https://stackoverflow.com/questions/77547060/fail-to-start-flask-connexion-swagger

--- rdf_differ/entrypoints/api/__init__.py
+++ rdf_differ/entrypoints/api/__init__.py
@@ -10,12 +10,16 @@ Module for configuring and exposing the connexion api server using the Flask fra
 """
 
 import connexion
+from connexion.options import SwaggerUIOptions
 
 from rdf_differ.config import RDF_DIFFER_SECRET_KEY_API, SHOW_SWAGGER_UI
 
-connexion_app = connexion.FlaskApp(__name__, specification_dir='openapi',
-                                   options={"swagger_ui": SHOW_SWAGGER_UI})
-connexion_app.add_api('openapi.yaml')
+swagger_ui_options = SwaggerUIOptions(swagger_ui=SHOW_SWAGGER_UI)
+
+connexion_app = connexion.FlaskApp(
+    __name__, specification_dir="openapi", swagger_ui_options=swagger_ui_options
+)
+connexion_app.add_api("openapi.yaml")
 
 app = connexion_app.app
 app.config['SECRET_KEY'] = RDF_DIFFER_SECRET_KEY_API
